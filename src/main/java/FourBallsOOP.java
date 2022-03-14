ffb386ef0088398ce8aa26ff5cb5d830c2cb0ff3
swananana <swanasimran977@gmail.com>
2022-03-15 03:19:01 +0530

    - initial commit -

---
+++ src/main/java/FourBallsOOP.java
@@ -0,0 +1,15 @@
+import processing.core.PApplet;
+
+public class FourBallsOOP extends PApplet{
+    public static int height = 488;
+    public static int width = 640;
+    public static int x1=0;
+    public static int x2=0;
+    public static int x3=0;public static int x4=0;
+    public static int diameter = 20;
+
+    public static void main(String... args){
+        PApplet.main("FourBallsSource");
+    }
+
+}
